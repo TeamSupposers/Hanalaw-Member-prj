50bcb65fa25518d14b00017d6a95be1560392b62
drow724 <drow724@naver.com>
2022-08-31 13:34:57 +0900

    컨텍스트 패스 추가

---
+++ src/main/java/com/member/configuration/ContextPathConfig.java
@@ -0,0 +1,29 @@
+package com.member.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.WebFilter;
+
+@Configuration
+public class ContextPathConfig {
+
+	@Value("${server.servlet.context-path}")
+    private String contextPath;
+	
+	@Bean
+	public WebFilter contextPathWebFilter() {
+	    return (exchange, chain) -> {
+	        ServerHttpRequest request = exchange.getRequest();
+	        if (request.getURI().getPath().startsWith(contextPath)) {
+	            return chain.filter(
+	                exchange.mutate()
+	                .request(request.mutate().contextPath(contextPath).build())
+	                .build());
+	        }
+	        return chain.filter(exchange);
+	    };
+	}
+
+}
